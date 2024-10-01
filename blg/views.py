8f24e4d45a4f1068f814d74de9661120797ec28b
VictorPerlaza <victorperlaza11b@gmail.com>
2024-10-01 10:22:14 -0500

    conexion bd
    se realizo la configuracion para configurar la bd

--- blg/views.py
+++ blg/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    print("Hola mundo")
+    return render(request='templates/index.html')
+
+def post_detail(request, id):
+    # Lógica para obtener el post por id y renderizar la plantilla
+    pass
