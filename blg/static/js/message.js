cf7db74ac648e0656881aba20788dfee3144af6c
VictorPerlaza <victorperlaza11b@gmail.com>
2024-10-07 17:14:40 -0500

    h
    h

---
+++ blg/static/js/message.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const messages = document.querySelectorAll('#floating-messages .alert');
+    messages.forEach((message) => {
+        setTimeout(() => {
+            message.style.opacity = '0'; // Comienza a desvanecerse
+            setTimeout(() => {
+                message.remove(); // Elimina el mensaje del DOM después del desvanecimiento
+            }, 500); // Tiempo de desvanecimiento
+        }, 3000); // Tiempo antes de comenzar a desvanecerse (en milisegundos)
+    });
+});
